23e882244be31a94e4ba74017a2ba20885162528
Harley Ehrman <harley.ehrman@outlook.com>
2018-05-31 22:21:31 -0400

    Add project files.

---
+++ RecentlyVisitedSites/RecentlyVisitedSites.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace EhrmanHarleyRecentlyVisitedSites
+{
+    partial class RecentlyVisitedSites
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblPoorlyDrawnLines = new System.Windows.Forms.LinkLabel();
+            this.lblTrashBird = new System.Windows.Forms.LinkLabel();
+            this.lblTheJoyOfSeex = new System.Windows.Forms.LinkLabel();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Controls.Add(this.lblPoorlyDrawnLines);
+            this.flowLayoutPanel1.Controls.Add(this.lblTrashBird);
+            this.flowLayoutPanel1.Controls.Add(this.lblTheJoyOfSeex);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(34, 31);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(253, 125);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Visible = false;
+            // 
+            // lblPoorlyDrawnLines
+            // 
+            this.lblPoorlyDrawnLines.AutoSize = true;
+            this.lblPoorlyDrawnLines.Location = new System.Drawing.Point(3, 0);
+            this.lblPoorlyDrawnLines.Name = "lblPoorlyDrawnLines";
+            this.lblPoorlyDrawnLines.Size = new System.Drawing.Size(198, 25);
+            this.lblPoorlyDrawnLines.TabIndex = 0;
+            this.lblPoorlyDrawnLines.TabStop = true;
+            this.lblPoorlyDrawnLines.Text = "Poorly Drawn Lines";
+            this.lblPoorlyDrawnLines.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.lblPoorlyDrawnLines.MouseHover += new System.EventHandler(this.linkLabel1_MouseHover);
+            // 
+            // lblTrashBird
+            // 
+            this.lblTrashBird.AutoSize = true;
+            this.lblTrashBird.Location = new System.Drawing.Point(3, 25);
+            this.lblTrashBird.Name = "lblTrashBird";
+            this.lblTrashBird.Size = new System.Drawing.Size(111, 25);
+            this.lblTrashBird.TabIndex = 0;
+            this.lblTrashBird.TabStop = true;
+            this.lblTrashBird.Text = "Trash Bird";
+            this.lblTrashBird.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            this.lblTrashBird.MouseHover += new System.EventHandler(this.linkLabel2_MouseHover);
+            // 
+            // lblTheJoyOfSeex
+            // 
+            this.lblTheJoyOfSeex.AutoSize = true;
+            this.lblTheJoyOfSeex.Location = new System.Drawing.Point(3, 50);
+            this.lblTheJoyOfSeex.Name = "lblTheJoyOfSeex";
+            this.lblTheJoyOfSeex.Size = new System.Drawing.Size(168, 25);
+            this.lblTheJoyOfSeex.TabIndex = 0;
+            this.lblTheJoyOfSeex.TabStop = true;
+            this.lblTheJoyOfSeex.Text = "The Joy of Seex";
+            this.lblTheJoyOfSeex.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            this.lblTheJoyOfSeex.MouseHover += new System.EventHandler(this.linkLabel3_MouseHover);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(344, 21);
+            this.lblDescription.MaximumSize = new System.Drawing.Size(300, 400);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(0, 25);
+            this.lblDescription.TabIndex = 1;
+            // 
+            // RecentlyVisitedSites
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "RecentlyVisitedSites";
+            this.Text = "RecentlyVisitedSites";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.LinkLabel lblPoorlyDrawnLines;
+        private System.Windows.Forms.LinkLabel lblTrashBird;
+        private System.Windows.Forms.LinkLabel lblTheJoyOfSeex;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
+
---
+++ RecentlyVisitedSites/RecentlyVisitedSites.cs
@@ -0,0 +1,85 @@
+﻿/********************************************************************
+ * Original from SDEV 240                                           *
+ *                                                                  *
+ * From book:                                                       *
+ * Microsoft Visual C# 2015:                                        *
+ * An Introduction to Object-Oriented Programming 6th Edition       *
+ * by Joyce Farrell                                                 *
+ *                                                                  *
+ * Instructions (Page 652, Exercise 2):                             *
+ * Create a project named RecentlyVisitedSites that contains a      *
+ * Form with a list of three LinkLabels that link to any three Web  *
+ * sites you choose. When a user clicks a LinkLabel, link to that   *
+ * . When a user’s mouse hovers over a LinkLabel, display a brief   *
+ * message that explains the site’s purpose. After a user clicks a  *
+ * link, move the most recently selected link to the top of the     *
+ * list, and move the other two links down, making sure to retain   *
+ * the correct explanation with each link.                          *
+ ********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EhrmanHarleyRecentlyVisitedSites
+{
+    public partial class RecentlyVisitedSites : Form
+    {
+        FlowLayoutPanel panel = new FlowLayoutPanel();
+
+
+        public RecentlyVisitedSites()
+        {
+            InitializeComponent();
+
+            panel.FlowDirection = FlowDirection.TopDown;
+
+            lblPoorlyDrawnLines.LinkClicked += linkLabel1_LinkClicked;
+            lblTrashBird.LinkClicked += linkLabel2_LinkClicked;
+            lblTheJoyOfSeex.LinkClicked += linkLabel3_LinkClicked;
+
+            Controls.Add(panel);
+            panel.Controls.Add(lblPoorlyDrawnLines);
+            panel.Controls.Add(lblTrashBird);
+            panel.Controls.Add(lblTheJoyOfSeex);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("IExplore", "http://www.poorlydrawnlines.com/");
+            panel.Controls.SetChildIndex(lblPoorlyDrawnLines, 0);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("IExplore", "https://www.webtoons.com/en/comedy/trash-bird/list?title_no=473&page=16");
+            panel.Controls.SetChildIndex(lblTrashBird, 0);
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("IExplore", "http://thejoyofseex.com/");
+            panel.Controls.SetChildIndex(lblTheJoyOfSeex, 0);
+        }
+
+        private void linkLabel1_MouseHover(object sender, EventArgs e)
+        {
+            lblDescription.Text = "Poorly Drawn Lines is a webcomic updated every Monday, Wednesday, and Friday.";
+        }
+
+        private void linkLabel2_MouseHover(object sender, EventArgs e)
+        {
+            lblDescription.Text = "Trash Bird is another comic written by Reza Farazmand.";
+        }
+
+        private void linkLabel3_MouseHover(object sender, EventArgs e)
+        {
+            lblDescription.Text = "The Joy of Seex is illustrations by Daniel Seex.";
+        }
+    }
+}
